c2033911f5041f452fe32dfeea283a019c88fd70
Judy-Choi <mjchoi0831@gmail.com>
2023-02-17 10:49:03 +0900

    FE/BE 초기세팅

---
+++ FrontEnd/src/pages/Npalce.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Npalce = () => {
+  return <button className="bg-sky-500 hover:bg-sky-700">Save changes</button>;
+};
+
+export default Npalce;
---
+++ FrontEnd/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        purple: "#4d52ff",
+        lightpurple: "#a8abff",
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
+  },
+};
