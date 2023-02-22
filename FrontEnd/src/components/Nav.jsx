9aa7e33ab43558db09bc913182048ad475acfd62
syoee <tjsdyd1347@gmail.com>
2023-02-22 11:43:16 +0900

    Add: header layout

--- FrontEnd/src/components/Nav.jsx
+++ FrontEnd/src/components/Nav.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import 'flowbite';
+import SelectStore from './SelectStore';
 const Nav = () => {
-  return <button className="bg-sky-500 hover:bg-sky-700">Save changes</button>;
+  return (
+    <div className="flex items-center pt-3 max-h-96">
+      <a href="/">
+        <img
+          className="mx-10 h-12"
+          src="https://user-images.githubusercontent.com/101877533/219550306-cde1c1ce-e029-40e9-8b05-de97db9e7cf9.jpg"
+          alt="logo"
+        />
+      </a>
+      <SelectStore />
+    </div>
+  );
 };
 
 export default Nav;
