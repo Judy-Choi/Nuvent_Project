9d3117ac018eda608a7dc531f788da2d25462022
syoee <tjsdyd1347@gmail.com>
2023-03-03 21:54:59 +0900

    Add: store

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
+import { Provider } from 'react-redux';
+import store from './modules';
 import 'tailwindcss/tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+root.render(
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
