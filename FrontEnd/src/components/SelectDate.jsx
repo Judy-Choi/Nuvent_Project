b53d4171df711bd0d9ff279fbc3524ec94f8a128
syoee <tjsdyd1347@gmail.com>
2023-03-06 11:46:19 +0900

    Modify: react-datepicker

--- FrontEnd/src/components/SelectDate.jsx
+++ FrontEnd/src/components/SelectDate.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-datepicker/dist/react-datepicker.min.css';
-import 'react-datepicker/dist/react-datepicker.min.js';
 import { useDispatch } from 'react-redux';
 import { setEndDate, setStartDate } from '../modules/reviewStore';
 
